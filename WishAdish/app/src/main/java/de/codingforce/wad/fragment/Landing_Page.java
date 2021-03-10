9df62d0df0a4b9a7468880a4ccaffa8063729aa6
Wambo04 <Timo.Kohlmeier@gmx.de>
2021-03-10 12:16:56 +0100

    adding Tabs

--- WishAdish/app/src/main/java/de/codingforce/wad/fragment/Landing_Page.java
+++ WishAdish/app/src/main/java/de/codingforce/wad/fragment/Landing_Page.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -17,15 +18,13 @@ import de.codingforce.wad.activity.MainActivity;
 import de.codingforce.wad.fragment.adapter.TabFragmentAdapter;
 
 public class Landing_Page extends NameAwareFragment {
-    private static final String LOG_TAG = "Landing Page";
-    private final String[] tabTexts = new String[]{"Songs", "Albums"};
 
-    /**
-     * Default constructor
-     */
-    public Landing_Page() {
-        setFragmentname(this.getClass().getSimpleName());
-    }
+    private static final String LOG_TAG = "Landing Page";
+    private final String[] tabTexts = new String[]{"Gerichte Heute", "Aktive Einkaufslisten"};
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private TabLayoutMediator mediator;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
@@ -36,6 +35,60 @@ public class Landing_Page extends NameAwareFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         Log.e(LOG_TAG, "--onViewCreated--");
-        ((MainActivity) getContext()).setTitle(R.string.landing);
+
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager2 = view.findViewById(R.id.landing_page_pager);
+
+        TabFragmentAdapter adapter = new TabFragmentAdapter(getChildFragmentManager(), this.getLifecycle(), (MainActivity) getActivity());
+        viewPager2.setAdapter(adapter);
+
+        TabLayoutMediator.TabConfigurationStrategy strategy = new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                // stub
+            }
+        };
+
+        mediator = new TabLayoutMediator(tabLayout, viewPager2, strategy);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Log.e(LOG_TAG, "--onTabSelected--");
+                MainActivity activity = (MainActivity) getActivity();
+                switch (tab.getPosition()) {
+                    case 0:
+                        // Songs
+                        Log.e(LOG_TAG, "set current tab: Dishes_Today");
+                        //activity.setCurrentTab(MainActivity.TAB_SONGS);
+                        activity.invalidateOptionsMenu();
+                        break;
+                    case 1:
+                        // Albums
+                        Log.e(LOG_TAG, "set current tab: Open_Shoppinglists");
+                        //activity.setCurrentTab(MainActivity.TAB_ALBUMS);
+                        activity.invalidateOptionsMenu();
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        mediator.attach();
+        this.initializeTabTexts();
+    }
+
+    private void initializeTabTexts() {
+        for (int i = 0; i < tabTexts.length; i++) {
+            tabLayout.getTabAt(i).setText(tabTexts[i]);
+        }
     }
 }
--- WishAdish/app/src/main/java/de/codingforce/wad/fragment/adapter/TabFragmentAdapter.java
+++ WishAdish/app/src/main/java/de/codingforce/wad/fragment/adapter/TabFragmentAdapter.java
@@ -4,7 +4,9 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
@@ -15,43 +17,39 @@ import de.codingforce.wad.fragment.tab.Open_Shoppinglists;
 /**
  * Adapter class to retrieve fragments for tabs
  */
-public class TabFragmentAdapter extends FragmentStateAdapter {
+public class TabFragmentAdapter extends FragmentStateAdapter{
     private static final String LOG_TAG = "TabFragmentAdapter";
+
     private final int numOfTabs;
     private final MainActivity activity;
 
-    /**
-     * Default constructor
-     * @param fragmentManager fragmentManager
-     * @param lifecycle lifecycle
-     * @param activity activity
-     */
     public TabFragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle, MainActivity activity) {
         super(fragmentManager, lifecycle);
-        numOfTabs = 3;
+        numOfTabs = 2;
         this.activity = activity;
     }
+
 
     @NonNull
     @Override
     public Fragment createFragment(int position) {
         Log.e(LOG_TAG, "-- createFragment --");
-        switch (position) {
+        switch (position)
+        {
             case 0:
-                // Wünsche heute
-                Log.e(LOG_TAG, "position:"+position+", creating "+Dishes_Today.class.getSimpleName());
+                Log.e(LOG_TAG, "position:"+position+", creating ");
                 return new Dishes_Today();
-            /*case 1:
-                //Einkauflisten
-                Log.e(LOG_TAG, "position:"+position+", creating "+Open_Shoppinglists.class.getSimpleName());
-                return new Dishes_Today();*/
+            case 1:
+                Log.e(LOG_TAG, "position:"+position+", creating ");
+                return new Open_Shoppinglists();
             default:
-                return null;
+                Log.e(LOG_TAG, "position:"+position+", creating ");
+                return new Dishes_Today();
         }
     }
 
     @Override
     public int getItemCount() {
-        return numOfTabs;
+        return 2;
     }
 }
